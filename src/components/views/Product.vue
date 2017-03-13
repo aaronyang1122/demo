34c5b836a91c24d32065ad46d5e99b9a8722d7b0
babyang <aaronyang1122@gmail.com>
2017-03-13 16:51:51 +0800

    update product temp

--- src/components/views/Product.vue
+++ src/components/views/Product.vue
@@ -1,113 +1,67 @@
 <template>
 	<section class="production">
-		<div class="full-width-container p1">
+
+		<div class="full-width-container" :class="{ 'layout-vertical': item.textposition === 'top' || item.textposition === 'bottom',  'layout-horizontal': item.textposition === 'left' || item.textposition === 'right'}" :style="{background: item.bgcolor}" v-for="(item, index) in detail.sections">
 		  <div class="inner">
-          <div class="container">
-            <img src="../../assets/image/p-1.png"/>
+          <div class="container" v-if="item.textposition === 'top' || item.textposition === 'bottom'">
+            <h2 :style="{color: item.titlecolor}">{{ item.title[$route.query['language']] }}</h2>
+            <p v-for="p in item.content[$route.query['language']].split(/\\s+|[;；]/g)">
+              {{ p }}
+            </p>
+            <img :src="item.img" v-if="item.img !== '/'"/>
           </div>
+
+          <div class="container" v-if="item.textposition === 'right'">
+            <div class="row">
+              <div class="col-sm-6 pic">
+                <img :src="item.img"/>
+              </div>
+              <div class="col-sm-6 text">
+                <h2 :style="{color: item.titlecolor}">{{ item.title[$route.query['language']] }}</h2>
+                <p v-for="p in item.content[$route.query['language']].split(/\\s+|[;；]/g)">
+                  {{ p }}
+                </p>
+              </div>
+            </div>
+          </div>
+
+          <div class="container" v-if="item.textposition === 'left'">
+            <div class="row">
+
+              <div class="col-sm-6 text">
+                <h2 :style="{color: item.titlecolor}">{{ item.title[$route.query['language']] }}</h2>
+                <p v-for="p in item.content[$route.query['language']].split(/\\s+|[;；]/g)">
+                  {{ p }}
+                </p>
+              </div>
+
+              <div class="col-sm-6 pic">
+                <img :src="item.img"/>
+              </div>
+
+            </div>
+          </div>
+
       </div>
 		</div>
 
-		<div class="full-width-container p2">
+   <!-- <div class="full-width-container layout-horizontal">
       <div class="inner">
           <div class="container">
             <template v-if="$route.query['language'] === 'en'">
             <div class="row">
-              <div class="col-sm-6 left">
-                <h2>Lightweight and convenient, beautiful and fashionable</h2>
-                <p>The total mass of 145 grams, comfortable to wear, outdoor wear support；</p>
 
-                <h2>large viewing angle, clear imaging</h2>
-                <p>FOV39 degree perspective, 3 meters outside the 70 inch through virtual screen;<br />
-1280x720 resolution, high saturation, high brightness distortion-free images;<br />
-Supports 2D/3D video playback</p>
-
-                <h2>Android integrated machine</h2>
-                <p>Support for Android application running;<br />Support indoor and outdoor broadcast function and photos, video, can share the shooting to social app;<br />Upload images and videos to the mobile and cloud by 5GWIFI, Bluetooth system；<br />Simultaneously display multiple windows in 360 space;</p>
-              </div>
-              <div class="col-sm-6">
-                <img src="../../assets/image/glass.png"/>
-              </div>
-            </div>
-            </template>
-            <template v-else>
-              <div class="row">
-              <div class="col-sm-6 left">
-                <h2>轻巧便捷，美观时尚</h2>
-                <p>总质量145克，佩戴舒适，支持户外穿戴</p>
-
-                <h2>超大视角，清晰成像</h2>
-                <p>拥有FOV39度视角，3米外100寸2D/3D穿透式虚拟屏幕；<br />
-1280x720分辨率，高饱和度、高亮度无失真图像效果；<br />
-完美支持2D/3D视频播放；</p>
-
-                <h2>Android一体机</h2>
-                <p>支持Android应用运行；<br />支持将拍摄的内容分享至微信、微博；<br />可以通过5GWIFI、蓝牙同云端（手机）连接传输照片和视频；<br />可实现在空间投放屏幕及素材，跟随视野的变化进行控制，可同时在360度空间中显示多个窗口。</p>
-              </div>
-              <div class="col-sm-6">
-                <img src="../../assets/image/glass.png"/>
-              </div>
-            </div>
-            </template>
-          </div>
-      </div>
-    </div>
-
-
-    <div class="full-width-container p3">
-      <div class="inner">
-        <template v-if="$route.query['language'] === 'en'">
-
-          <div class="container">
-            <h2>PERFORMANCE</h2>
-            <p>EST Customized chip set；A1,64-bit 4-core； Cortex-A53 Chip Architecture；  CPU 2.0GHZ, GPU Mali-450； </p>
-            <p>NADN+LPDDR3 32G+2G/64+4G Can be selected;</p>
-<p>2.4G/5GDual band WiFi, Support IEEE 802.11a/b/g/n/ac；</p>
-<p>Bluetooth V4.0 BLE+EDR The downward compatibility of wireless mouse and keyboard support；</p>
-
-<p>TOF support 3D scanning;The spatial modeling and spatial localization; Support gesture recognition and 720P video recording；</p>
-<p>PNI High precision 3 axis geomagnetic sensor +BOSCH 6 axis sensors; Head tracking function；</p>
-<p>PNI High precision 3 axis geomagnetic sensor +BOSCH 6 axis sensors; Head tracking function；</p>
-<p>Voice recognition input& Touch Pad input；</p>
-<p>Opto-mechanical 1280x720@60HZ; FOV 39 degrees angle; See through ; 70 inches screen 3meters away; 2D/3D AR display.</p>
-
-          </div>
-          </template>
-        <template v-else>
-          <div class="container">
-            <h2>性能</h2>
-            <p>EST 定制芯片A1,64-bit 四核 Cortex-A53架构  </p>
-            <p>主频2.0GHZ, GPU Mali-450；</p>
-<p>2.4G/5G双频WIFI,支持IEEE 802.11a/b/g/n/ac；</p>
-<p>Bluetooth V4.0 BLE+ED¬R 向下兼容,支持无线鼠标和键盘；</p>
-
-<p>TOF 支持3D扫描,实现空间建模和空间定位;支持手势识别和720P视频录制；</p>
-<p>PNI 3轴高精度地磁传感器+BOSCH 6轴传感器,实现头部跟踪功能；</p>
-<p>双电池1000mAH+600mAH,支持长达3小时的视频播放和应用；</p>
-<p>支持语音识别输入和TP（触摸板）触摸控制；</p>
-<p>光机 1280x720@60HZ,FOV 39度视角,3米100寸穿透式2D/3D虚拟显示。</p>
-
-      </div>
-          </template>
-      </div>
-    </div>
-
-    <div class="full-width-container p4">
-      <div class="inner">
-          <div class="container">
-            <template v-if="$route.query['language'] === 'en'">
-            <div class="row">
-              <div class="col-sm-6">
-                <img src="../../assets/image/p-4.jpg"/>
-              </div>
               <div class="col-sm-6 left">
                 <h2>Hands Free</h2>
                 <p>Important information in outdoor activities can be displayed at any time;</p>
-<p>keep the information communication between the team members;</p>
-<p>Picture or video directly view other members by shooting glasses；（2D/3D）</p>
-<p>Navigation information while driving directly in the line of sight, the best experience of the implementation of auxiliary information;</p>
-<p>Navigation information while driving directly in the line of sight, the best experience of the implementation of auxiliary information;</p>
+                <p>keep the information communication between the team members;</p>
+                <p>Picture or video directly view other members by shooting glasses；（2D/3D）</p>
+                <p>Navigation information while driving directly in the line of sight, the best experience of the implementation of auxiliary information;</p>
+                <p>Navigation information while driving directly in the line of sight, the best experience of the implementation of auxiliary information;</p>
+              </div>
 
+              <div class="col-sm-6">
+                <img src="../../assets/image/p-4.jpg"/>
               </div>
 
             </div>
@@ -115,17 +69,19 @@ Supports 2D/3D video playback</p>
             <template v-else>
 
             <div class="row">
-              <div class="col-sm-6">
-                <img src="../../assets/image/p-4.jpg"/>
-              </div>
-              <div class="col-sm-6 left">
+
+              <div class="col-sm-6 text">
                 <h2>解放双手</h2>
                 <p>户外活动中重要的信息可以随时在眼前显示；</p>
-<p>保持团队间的信息交流；</p>
-<p>直接查看其他成员眼镜所拍摄的画面或视频；</p>
-<p>驾车时的导航信息直接在视线中显示，实现最佳体验的信息辅助；</p>
-<p>同时支持语音输入的功能，避免在开车时用手机输入导致的危险。</p>
+                <p>保持团队间的信息交流；</p>
+                <p>直接查看其他成员眼镜所拍摄的画面或视频；</p>
+                <p>驾车时的导航信息直接在视线中显示，实现最佳体验的信息辅助；</p>
+                <p>同时支持语音输入的功能，避免在开车时用手机输入导致的危险。</p>
 
+              </div>
+
+              <div class="col-sm-6 pic">
+                <img src="../../assets/image/p-4.jpg"/>
               </div>
 
             </div>
@@ -136,44 +92,63 @@ Supports 2D/3D video playback</p>
       </div>
     </div>
 
-
-    <div class="full-width-container p5">
+    <div class="full-width-container layout-horizontal ">
       <div class="inner">
-        <template v-if="$route.query['language'] === 'en'">
-
-          <div class="container">
-            <h2>Holographic Display</h2>
-            <p>Getting common information in holographic way, more intuitive and more interesting; </p>
-            <p>can simulate the effect of objects placed in a real environment, such as furniture shopping, online shopping and measurement;</p>
-<p>Support AR Game & AR education APPs；</p>
-<p>Virtual pets, decorations and Realization of intelligent virtual Secretary.</p>
-
-          </div>
-
-          </template>
-        <template v-else>
+        <div class="container">
 
 
+            <div class="row">
 
-          <div class="container">
-            <h2>全息展示</h2>
-            <p>将常用信息以全息的方式展现，更直观更有趣； </p>
-            <p>可模拟物体在真实环境下摆放的效果，如家具选购，网络购物及测量；</p>
-<p>各类AR教育及游戏应用的运行；</p>
-<p>虚拟宠物，装饰品以及智能虚拟秘书的实现（需人工智能及应用开发的配合）。</p>
+              <div class="col-sm-6 pic">
+                <img src="../../assets/image/p-4.jpg"/>
+              </div>
 
-          </div>
+              <div class="col-sm-6 text">
+                <h2>解放双手</h2>
+                <p>户外活动中重要的信息可以随时在眼前显示；</p>
+                <p>保持团队间的信息交流；</p>
+                <p>直接查看其他成员眼镜所拍摄的画面或视频；</p>
+                <p>驾车时的导航信息直接在视线中显示，实现最佳体验的信息辅助；</p>
+                <p>同时支持语音输入的功能，避免在开车时用手机输入导致的危险。</p>
+              </div>
 
-          </template>
+
+
+            </div>
+
+
+        </div>
       </div>
-    </div>
+    </div>-->
 
 	</section>
 </template>
-<script>
+<script type="text/javascript">
 	export default {
+	  data () {
+      return {
+        detail: {}
+      }
+    },
 	  created () {
 //	    document.body.scrollTop=0;
+      this.$http.get(
+          '/api/product/detail',
+          {
+            params: {
+                id: this.$route.params.id
+            }
+          }
+        )
+        .then(
+          (res) => {
+              this.detail = res.body
+              console.log(this.detail)
+          },
+          () => {
+            this.detail = {}
+          }
+        )
 	  }
 	}
 </script>
@@ -183,63 +158,58 @@ Supports 2D/3D video playback</p>
     overflow: hidden;
   }
 	.full-width-container {
-	  margin-bottom: 20px;
 	  &:first-child {
 	    margin-top: 50px;
 	  }
 	  .container {
-
+      color: #333;
 	  }
 	}
-	.p1 {
-	  background-color: #eaa949;
+	.layout-vertical {
 	  .container {
       text-align: center;
-      padding: 80px 0;
+      padding: 60px 0;
       img {
-        width: 50%;
+        width: 80%;
+      }
+      h2 {
+        font-size: 36px;
+        margin-bottom: 24px;
+        padding: 0 20px;
+      }
+      p {
+        padding: 0 20px;
       }
     }
 	}
 
-	.p2 {
-    background-color: #ecedf1;
-    .container {
-      padding: 80px 0;
-      text-align: center;
-      img {
-        width: 80%;
-        margin-top: 120px;
-      }
-      .left {
-        text-align: left;
-        padding: 0 60px;
-      }
-      h2 {
-        color: #eaa949;
-        font-size: 24px;
-      }
-    }
-  }
-
-  .p4 {
-    background-color: #ecedf1;
+  .layout-horizontal {
     .container {
       padding: 0;
       text-align: center;
       .row {
         margin-bottom: 0;
-        img {
-          width: 100%;
-          display: block;
+        .pic {
+          padding: 0;
+          img {
+            width: 100%;
+            display: block;
+          }
         }
-        .left {
-          text-align: left;
-          padding: 40px 40px;
+        .text {
+          text-align: center;
+          padding: 0 40px;
         }
         h2 {
           color: #eaa949;
-          font-size: 24px;
+          font-size: 36px;
+          margin: 30px 0;
+        }
+        p {
+          margin-bottom: 3px;
+          &:last-of-type {
+            margin-bottom: 20px;
+          }
         }
       }
 
@@ -321,51 +291,55 @@ Supports 2D/3D video playback</p>
   }
 
   @media (min-width: 768px) {
-    .p3 {
-      padding-top: 50%;
-      height: 0;
-    overflow: hidden;
-    .inner {
-      position: absolute;
+    .layout-vertical {
       .container {
-        p {
-          text-align: center;
+        text-align: center;
+        padding: 60px 0;
+        img {
+          width: 50%;
+        }
+        h2 {
+          font-size: 36px;
+          margin-bottom: 24px;
         }
       }
     }
-    }
 
-    .p4 {
-    .container {
-      .row {
-        .left {
-          padding: 0 40px;
-          margin-top: 100px;
-      }
-      }
-    }
-  }
-
-
-    .p5 {
-      padding-top: 50%;
-      height: 0;
-    overflow: hidden;
-    .inner {
-      top: 50%;
-      position: absolute;
+    .layout-horizontal {
       .container {
-        p {
-          text-align: center;
+        padding: 0;
+        text-align: center;
+        .row {
+          display: flex;
+          align-items: center;
+          margin-bottom: 0;
+          .pic {
+            padding: 0;
+            img {
+              width: 100%;
+              display: block;
+            }
+          }
+          .text {
+            text-align: left;
+            padding: 0;
+          }
+          h2 {
+            color: #eaa949;
+            font-size: 36px;
+            margin-bottom: 30px;
+            padding-left: 40px;
+          }
+          p {
+            margin-bottom: 6px;
+            padding-left: 40px;
+          }
         }
+
       }
     }
-    }
+
   }
 
-  @media (min-width: 1400px) {
-    .p3 .p5{
-      padding-top: 800px;
-    }
-  }
+
 </style>
