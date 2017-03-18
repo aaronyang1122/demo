98d5ffd0b40dbce3b5b9da8b83318e73b8e6ec1a
yangjian <aaronyang1122@gmail.com>
2017-03-18 13:19:07 +0800

    update index and main

--- src/main.js
+++ src/main.js
@@ -3,8 +3,12 @@ import App from './App'
 import router from './router/index'
 import store from './store'
 import VueResource from 'vue-resource'
+import vMediaQuery from 'v-media-query'
+
 
 Vue.use(VueResource)
+
+Vue.use(vMediaQuery)
 
 /* eslint-disable no-new */
 new Vue({
